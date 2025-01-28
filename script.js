56a1b5225eeb105df5a7a2fb79b531b3daa3eb7f
Durga Sai <dsainvg.20.12.24@kgpian.iitkgp.ac.in>
2025-01-28 19:17:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const game = document.getElementById('game');
 let blockNo = 1;
 let score = 0;
 let lives = 3;
+let a=1500;
+let perBlocks = 8;
+let lifetakerCount = 0;
+let badblocks = 0;
 
 const firstbrick = document.createElement('div');
 firstbrick.id = 'bricks';
@@ -24,19 +28,17 @@ lifetaker.appendChild(lifetakerImg);
 function updateBasket(a) {
     var left = parseInt(window.getComputedStyle(basket).getPropertyValue('left'));
     if((left +a >20)&&(left +a < 850)){
-    console.log(left+a);
     basket.style.left = (left + a) + 'px';
     }
 }
-a=1500;
 document.addEventListener('keydown', event => {
     if ((event.key === 'ArrowLeft')||(event.key === "A")||(event.key === "a")) {
-        console.log("LEFT");
+      
         updateBasket(-10);
     }
     if ((event.key === 'ArrowRight')||(event.key === "D")||(event.key === "d")) {
         updateBasket(10);
-        console.log("RIGHT");
+        
     }
 });
   
@@ -59,7 +61,18 @@ function executeAtInterval(callback, interval) {
 
 executeAtInterval(() => {
     if(lives>0){
-    generateBricks();}
+        if(lifetakerCount>0){
+            generateBricks();
+        }
+        else{
+            if(blockNo%perBlocks==0){
+                generateBlack();
+            }
+            else{
+                generateBricks(); 
+            } 
+        }
+    }
 }, a);
 
 function generateBricks(){
@@ -67,15 +80,13 @@ function generateBricks(){
     newbrick.style.left = ran() + 'px';
     game.appendChild(newbrick);
     blockNo++;
-    if(blockNo%10==0){
-        generateBlack();
-    }
+    lifetakerCount = 0;
     moveBricks(newbrick);
 }
 
 function moveBricks(newbrick){
     var top = -100;
-    var id = setInterval(frame, 10);
+    var id = setInterval(frame, timeing());
     function frame() {
         if (top > 300) {
             clearInterval(id);
@@ -83,11 +94,17 @@ function moveBricks(newbrick){
             basketleft = parseInt(window.getComputedStyle(basket).getPropertyValue('left'));
             if (left > basketleft-100 && left < basketleft + 100) {
                 score++;
+                newbrick.style.height = '90px';
+                if((score%5==0)&&(a>500)){
+                        a=a-80;}
+                if((score%5==0)&&(perBlocks>3)){
+                        perBlocks=perBlocks-1;}
                 
                 document.getElementById('score-count').innerHTML = score;
             }
             newbrick.remove();
-        } else {
+        }
+        else {
             top++;
             newbrick.style.top = top + 'px';
         }
@@ -99,11 +116,12 @@ function generateBlack(){
     newbrick.style.left = ran() + 'px';
     game.appendChild(newbrick);
     movelife(newbrick);
+    lifetakerCount = lifetakerCount + 1;
 }
 
 function movelife(newbrick){
     var top = -100;
-    var id = setInterval(frame, 10);
+    var id = setInterval(frame, timeing());
     function frame() {
         if (top > 300) {
             clearInterval(id);
@@ -114,6 +132,8 @@ function movelife(newbrick){
                 checklives();
             }
             newbrick.remove();
+            badblocks++;
+            console.log(badblocks);
         } else {
             top++;
             newbrick.style.top = top + 'px';
@@ -124,15 +144,15 @@ function movelife(newbrick){
 function checklives(){
     if(lives==2){
         const liveImgCh = document.getElementById('lives-count2')
-        liveImgCh.innerHTML = `<svg width="40px" height="40px" viewBox="-3.36 -3.36 22.72 22.72" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"/><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/><g id="SVGRepo_iconCarrier"> <path d="M8 15L1.24264 8.24264C0.44699 7.44699 0 6.36786 0 5.24264V5.05234C0 2.8143 1.8143 1 4.05234 1C5.14512 1 6.1853 1.44095 6.94309 2.2124L5.85583 6.2701L7.7832 8.19746L7.10553 9.55279L8.89438 10.4472L10.2167 7.80255L8.14408 5.72991L9.74447 1.65122C10.3932 1.23104 11.1567 1 11.9477 1C14.1857 1 16 2.8143 16 5.05234V5.24264C16 6.36786 15.553 7.44699 14.7574 8.24264L8 15Z" fill="red"/> </g></svg>`;
+        heartbreak(liveImgCh);
     }
     if(lives==1){
         const liveImgCh = document.getElementById('lives-count1')
-        liveImgCh.innerHTML = `<svg width="40px" height="40px" viewBox="-3.36 -3.36 22.72 22.72" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"/><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/><g id="SVGRepo_iconCarrier"> <path d="M8 15L1.24264 8.24264C0.44699 7.44699 0 6.36786 0 5.24264V5.05234C0 2.8143 1.8143 1 4.05234 1C5.14512 1 6.1853 1.44095 6.94309 2.2124L5.85583 6.2701L7.7832 8.19746L7.10553 9.55279L8.89438 10.4472L10.2167 7.80255L8.14408 5.72991L9.74447 1.65122C10.3932 1.23104 11.1567 1 11.9477 1C14.1857 1 16 2.8143 16 5.05234V5.24264C16 6.36786 15.553 7.44699 14.7574 8.24264L8 15Z" fill="red"/> </g></svg>`;
+        heartbreak(liveImgCh);
     }
     if(lives==0){
         const liveImgCh = document.getElementById('lives-count0')
-        liveImgCh.innerHTML = `<svg width="40px" height="40px" viewBox="-3.36 -3.36 22.72 22.72" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"/><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/><g id="SVGRepo_iconCarrier"> <path d="M8 15L1.24264 8.24264C0.44699 7.44699 0 6.36786 0 5.24264V5.05234C0 2.8143 1.8143 1 4.05234 1C5.14512 1 6.1853 1.44095 6.94309 2.2124L5.85583 6.2701L7.7832 8.19746L7.10553 9.55279L8.89438 10.4472L10.2167 7.80255L8.14408 5.72991L9.74447 1.65122C10.3932 1.23104 11.1567 1 11.9477 1C14.1857 1 16 2.8143 16 5.05234V5.24264C16 6.36786 15.553 7.44699 14.7574 8.24264L8 15Z" fill="red"/> </g></svg>`;
+        heartbreak(liveImgCh);
         endcredits();
     }
     
@@ -142,6 +162,18 @@ function endcredits(){
     game.innerHTML =   `<div id="endcredits">
                         <h1>Game Over</h1>
                         <h1>Your Score is ${score}</h1>
+                        <h2>you had accuracy of ${Math.floor((score/blockNo)*10000)/100}%</h2>
                         <button class="reload" onclick="location.reload()">Play Again</button>
                         </div>`;    
+}
+
+function timeing(){
+    if(a<800){
+        return 3;
+    }
+    return Math.floor((a/100)-5);
+}
+
+function heartbreak(heartImage){
+    heartImage.innerHTML = `<svg width="40px" height="40px" viewBox="-3.36 -3.36 22.72 22.72" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"/><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/><g id="SVGRepo_iconCarrier"> <path d="M8 15L1.24264 8.24264C0.44699 7.44699 0 6.36786 0 5.24264V5.05234C0 2.8143 1.8143 1 4.05234 1C5.14512 1 6.1853 1.44095 6.94309 2.2124L5.85583 6.2701L7.7832 8.19746L7.10553 9.55279L8.89438 10.4472L10.2167 7.80255L8.14408 5.72991L9.74447 1.65122C10.3932 1.23104 11.1567 1 11.9477 1C14.1857 1 16 2.8143 16 5.05234V5.24264C16 6.36786 15.553 7.44699 14.7574 8.24264L8 15Z" fill="red"/> </g></svg>`;
 }
